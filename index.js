a49182b2a8e912c2160f75e8dbe259366504771a
mcferrenm <mcferrenmax@gmail.com>
2019-02-14 18:29:22 -0500

    add comment

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ server.get("/", (req, res) => {
   res.status(200).send("Welcome to Node Blog API");
 });
 
+// port
+
 server.listen(PORT, (req, res) => {
   console.log(`\n\n*** Listening on port ${PORT} ***\n`);
 });
